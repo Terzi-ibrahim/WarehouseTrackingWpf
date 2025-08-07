6df7c418674f228574c6a84d22330818325ab4ec
Terzi-ibrahim <ibrahim.terzi.41@hotmail.com>
2025-08-07 11:38:15 +0300

    database startet

---
+++ WarehouseTrackingWpf/Data/DatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using System.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseTrackingWpf.Data
+{
+    class DatabaseContext
+    {
+        private readonly string _connectionString;
+
+        public DatabaseContext()
+        {
+            _connectionString = ConfigurationManager
+                .ConnectionStrings["DefaultConnection"]
+                .ConnectionString;
+        }
+
+        public SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+    }
+}
